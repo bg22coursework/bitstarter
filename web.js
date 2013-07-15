7b49b4c14deecd7561e27f8f17bb9f4fb3d906ec
bg22coursework <bg22coursework@outlook.com>
2013-07-15 07:24:04 +0000

    updated to read from file sync

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
+var fs = require('fs');
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World Heroku!');
+  var buffer = fs.readFileSync("index.html");
+  response.send(buffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
